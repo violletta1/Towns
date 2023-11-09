f426b98db97c09014d0e24e0ec05b3c6476c729f
violletta1 <villygeorgieva@yahoo.com>
2023-11-09 17:20:39 +0000

    improve the HTML Structure

--- towns.js
+++ towns.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 	$('#btnDelete').click(deleteTown);
 	$('#btnAdd').click(addTown);
-	$('#btnShuffle').click(shuffleTowns);
 });
 
 function deleteTown() {
@@ -15,25 +14,20 @@ function deleteTown() {
 		}
 	}
 	if (removed)
-		$('#result').text(townName + " deleted.");
+		showMessage(townName + " deleted.");
 	else
-		$('#result').text(townName + " not found.");
+		showMessage(townName + " not found.");
 }
+
+function showMessage(msg) { 
+	$('#result').text(msg).css("display", "block"); 
+	setTimeout(function () {
+		 $('#result').hide('blind', {}, 500); 
+		}, 3000); 
+	}
+
 function addTown() { let townName = $('#townNameForAdd').val();
 $('#townNameForAdd').val('');
 $('#towns').append($('<option>').text(townName));
 $('#result').text(townName + " added.");
 }
-
-function shuffleTowns() {
-	 let towns = $('#towns option').toArray();
-	 $('#towns').empty();shuffleArray(towns);
-	 $('#towns').append(towns); $('#result').text("Towns shuffled.");
-	 
-	 function shuffleArray(array) { 
-		for (var i = array.length - 1; i > 0; i--) { 
-			var j = Math.floor(Math.random() * (i + 1));
-			var oldElement = array[i];
-			array[i] = array[j];
-			array[j] = oldElement; } }
-		}
